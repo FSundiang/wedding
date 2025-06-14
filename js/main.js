4ed9d5733c26cae1c8733cc6b632c7e4ab1ccd61
FSundiang <sundiang.franciscarl@gmail.com>
2025-06-14 19:50:55 +0800

    first-commit

--- js/main.js
+++ js/main.js
@@ -74,7 +74,7 @@ const initNavigation = () => {
 //toggle Menu
 const toggleMenu = () => {
     const menu = document.querySelector('.menu');
-    const navMobile = document.querySelector('nav-mobile');
+    const navMobile = document.querySelector('.nav-mobile');
     menu.classList.toggle('active');
     navMobile.classList.toggle('active');
 };
